d86944c314e5bdce1b70d3588081ea6ad9433d07
geoffrey riviere <geoffrey.riviere@ecole-it.com>
2024-03-19 12:48:12 +0100

    rajout fichier style.css

--- index.php
+++ index.php
@@ -4,13 +4,14 @@
     <meta charset="UTF-8">
     <title>Geoffrey Riviere</title>
     <link rel="icon" href="image/bussiness-man.png" type="image/png">
-    <link rel="stylesheet" href="css/style.css">
-    <link rel="stylesheet" href="css/competence.css">
-    <link rel="stylesheet" href="css/project.css">
-    <link rel="stylesheet" href="css/footer.css">
-    <link rel="stylesheet" href="css/experience.css">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link rel="stylesheet" href="./css/style.css">
+    <link rel="stylesheet" href="./css/competence.css">
+    <link rel="stylesheet" href="./css/project.css">
+    <link rel="stylesheet" href="./css/experience.css">
+    <link rel="stylesheet" href="./css/footer.css">
+    <link rel="stylesheet" href="./css/responsive.css">
+    <link rel="preconnect" href="./https://fonts.googleapis.com">
+    <link rel="preconnect" href="./https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Merriweather:wght@300&family=Roboto:wght@300&display=swap" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.3.0/flowbite.min.css"  rel="stylesheet" />
